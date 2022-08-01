b327696b1b2d3082b31d19c090b9547791b19789
Yash Sharma <86138890+Yashkr00129@users.noreply.github.com>
2022-08-02 03:59:16 +0530

    Make Single Project Page

---
+++ client/src/queries/projectQueries.js
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client"
+
+export const GET_PROJECTS = gql`
+  query GetProjects {
+    projects {
+      id
+      name
+      status
+    }
+  }
+`;
+
+export const GET_PROJECT = gql`
+  query GetProject($id: ID!) {
+    project(id: $id) {
+      id
+      name
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }`
